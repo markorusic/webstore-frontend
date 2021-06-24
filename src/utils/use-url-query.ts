3ca677cb54c6116b8cfec6ea931d3460a86c5ce1
Marko Rusic <markousp5@gmail.com>
2021-06-24 22:50:03 +0200

    rm clgs

--- src/utils/use-url-query.ts
+++ src/utils/use-url-query.ts
@@ -9,7 +9,6 @@ export const useURLQuery = <T>(initialValues?: T) => {
     ...Object.fromEntries(new URLSearchParams(location.search))
   } as T
   const setParam = (name: keyof T, value: string | number) => {
-    console.log('setParam', name, value)
     const newParams = Object.entries({
       ...params,
       [name]: value
@@ -20,14 +19,12 @@ export const useURLQuery = <T>(initialValues?: T) => {
   }
 
   const setParams = (updatedParams: any) => {
-    console.log('setParams', updatedParams)
     const newParams = Object.entries({
       ...params,
       ...updatedParams
       // @ts-ignore
     }).reduce((a, [k, v]) => (v == null ? a : ((a[k] = v), a)), {})
     const search = new URLSearchParams(newParams).toString()
-    // console.log('da2', newParams, search)
     history.push({ search })
   }
 
