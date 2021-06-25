02b2d759c60494380f81e2e50bd7a92cef525b1b
Marko Rusic <markousp5@gmail.com>
2021-06-25 12:31:38 +0200

    remove category data mockup

--- src/services/category-service.ts
+++ src/services/category-service.ts
@@ -7,16 +7,7 @@ export const categoryService = {
     const { data } = await http.get<Page<CategoryDto>>('/categories/findAll', {
       params
     })
-    return {
-      ...data,
-      content: data.content.map(category => ({
-        ...category,
-        photo:
-          Math.random() > 0.5
-            ? 'http://vuzz-backend.novafabrika.rs/upload/content/images/1608207957973-870264a5-2aa1-4418-8947-ca89164ad809.jpg'
-            : 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg'
-      }))
-    } as Page<CategoryDto>
+    return data
   }
 }
 
